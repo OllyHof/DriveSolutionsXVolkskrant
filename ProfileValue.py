157b29e7ff10c7cb5fe8fcb0db00cc5af69eb7fc
OllyHof <olly.hof@outlook.com>
2025-10-17 19:44:52 +0200

    Brute forcing T1

---
+++ ProfileValue.py
@@ -0,0 +1,27 @@
+import math as m
+
+ProfileValue = 4.5
+Vm = 0.25 # m/s
+Sm = 0.2   # m
+Tm = 1.0 # s
+
+while True:
+    T1 = Tm / ProfileValue
+    A = Vm / T1
+    Straveld = Vm * (Tm - (2 *T1)) +  0.5* A * T1**2 +  0.5* A * T1**2
+
+    print(f"T1: {T1:.4f}[s] | A: {A:.4f}[m/s²] | S: {Straveld:.9f}[m] | P: {ProfileValue:.4f} [-]")
+
+    if abs(Straveld - Sm) < 0.000000001:
+        print("Exact")
+        print(ProfileValue)
+        break
+    elif (Sm - 0.001) < Straveld < (Sm + 0.001):
+        print("Bijna")
+        ProfileValue = ProfileValue + 0.000000001
+    elif Straveld < Sm:
+        print("Te weinig")
+        ProfileValue = ProfileValue + 0.0001
+    else:
+        print("Te veel")
+        break
